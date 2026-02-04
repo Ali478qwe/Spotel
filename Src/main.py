13125f9c8aad613170a402e69d790b666dcd72d7
ALI478QWE <107773693+Ali478qwe@users.noreply.github.com>
2026-02-04 04:10:22 +0330

    Update main.py

--- Src/main.py
+++ Src/main.py
@@ -1,11 +1,23 @@
-from telethon import TelegramClient,events
-from config import *
+from telethon import TelegramClient, events
 import yt_dlp
 import os
+from config import *
 
-bot = TelegramClient('Spotel',api_id,api_hash).start(bot_token=bot_token)
+
+bot = TelegramClient(None, api_id, api_hash)
 
 @bot.on(events.NewMessage(pattern='/start'))
-async def start(events):
-    await events.respond('Hi Im Spotel Lets Start send link')
+async def start(event):
+    await event.respond('Hi! I am Spotel. Send me a link to start.')
 
+async def main():
+    
+    await bot.start(bot_token=bot_token)
+    print("Bot is running...")
+    
+
+    await bot.run_until_disconnected()
+
+if __name__ == '__main__':
+    import asyncio
+    asyncio.run(main())
