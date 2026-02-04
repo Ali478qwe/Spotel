f244f7801d74aaf0a90f99bf6061418939adf2a5
unknown <a667908@gmail.com>
2026-02-04 02:34:26 -0800

    fix Type error config

--- Src/config.py
+++ Src/config.py
@@ -2,6 +2,6 @@ import os
 # from dotenv import load_dotenv
 # BOT TOKEN - Require
 # load_dotenv()
-API_ID = int(os.getenv('API_ID'))  
-API_HASH = os.getenv('API_HASH')  
+# API_ID = int(os.getenv('API_ID'))  
+# API_HASH = os.getenv('API_HASH')  
 BOT_TOKEN = os.getenv('BOT_TOKEN') 
